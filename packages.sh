acf885c126a3d958a887961112bee742c8eea85b
Emin Eker <emineker@gmail.com>
2016-05-29 21:51:47 +0300

    update package list

--- packages.sh
+++ packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install -y git curl ssh vim htop lm-sensors zip rar vlc gimp google-chrome-stable
+sudo apt-get install -y git curl ssh vim htop ncdu lm-sensors zip rar vlc gimp google-chrome-stable
